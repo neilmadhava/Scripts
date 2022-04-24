5994d384c959aae185da92cb990471507b1fa682
neilmadhava <mukunda.madhava000@gmail.com>
2022-04-25 00:14:49 +0530

    Adding listal image download script

--- listal.sh
+++ listal.sh
@@ -5,6 +5,8 @@ read link
 echo -n "Enter number of pages: "
 read pages
 
+celeb=`echo $link | cut -d"/" -f4`
+
 for i in $(seq 1 $pages); do 
 	req_link="$link/$i"
 	results=`curl -s -i $req_link -A 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0' -H 'Cookie: s=abbde6ca83b00b366df00e5333e4dd7a; t=703e2206eb3c09d8fdd60bad923a05bd;ls=um582knel3mube4guq9t16mhvg' | grep -Po 'https://www.listal.com/viewimage/\K[^"\x27]+'`
@@ -13,13 +15,11 @@ for i in $(seq 1 $pages); do
 	echo "Downloading page $i: $num_result images"	
 	if [ $num_result != 1 ]; then
 		echo "$results" | while read line; do
-			img_link="https://ilarge.lisimg.com/image/$line/1080full-amy-aela.jpg"
-			if [ $img_link != "https://ilarge.lisimg.com/image/0/1080full-amy-aela.jpg" ]; then
+			img_link="https://ilarge.lisimg.com/image/$line/1080full-$celeb.jpg"
+			if [ $img_link != "https://ilarge.lisimg.com/image/0/1080full-$celeb.jpg" ]; then
 				wget $img_link -q --show-progress -O "$line.jpg" 
 				sleep 1
 			fi
 		done
 	fi
 done
-
-
